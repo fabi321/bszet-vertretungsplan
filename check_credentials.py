6ba1ac21f4f619b20b072f53abbfaacc50775781
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-01-23 17:28:15 +0100

    removed cred hints

--- check_credentials.py
+++ check_credentials.py
@@ -2,10 +2,6 @@ import re
 
 import requests
 
-# potential regular expressions for sanity check. Might be too strict
-USERNAME_REGEX: re.Pattern = re.compile(r'^bsz-et-[0-9]{4}$')
-PASSWORD_REGEX: re.Pattern = re.compile(r'^[a-z]+#[0-9]{2}$')
-
 
 def check(username: str, password: str) -> bool:
     if len(username) > 20 or len(password) > 20:
