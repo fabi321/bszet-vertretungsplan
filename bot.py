bc77dee0d9529a86eaa2d674e67d207948a6004e
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-01-24 14:39:12 +0100

    custom error handler

--- bot.py
+++ bot.py
@@ -1,15 +1,22 @@
 from os import getenv
 from pathlib import Path
+import logging
+import html
+import json
+import traceback
 
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import Application, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+from telegram.constants import ParseMode
 
 import check_credentials
 import update_substitutions
 from DB import DB
 
 global db
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 async def verify(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -68,6 +75,34 @@ async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Sorry, I didn't understand that command.")
 
 
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Log the error and send a telegram message to notify the developer."""
+    # Log the error before we do anything else, so we can see it even if something breaks.
+    logger.error(msg="Exception while handling an update:", exc_info=context.error)
+
+    # traceback.format_exception returns the usual python message about an exception, but as a
+    # list of strings rather than a single string, so we have to join them together.
+    tb_list = traceback.format_exception(None, context.error, context.error.__traceback__)
+    tb_string = "".join(tb_list)
+
+    # Build the message with some markup and additional information about what happened.
+    # You might need to add some logic to deal with messages longer than the 4096 character limit.
+    update_str = update.to_dict() if isinstance(update, Update) else str(update)
+    message = (
+        f"An exception was raised while handling an update\n"
+        f"<pre>update = {html.escape(json.dumps(update_str, indent=2, ensure_ascii=False))}"
+        "</pre>\n\n"
+        f"<pre>context.chat_data = {html.escape(str(context.chat_data))}</pre>\n\n"
+        f"<pre>context.user_data = {html.escape(str(context.user_data))}</pre>\n\n"
+        f"<pre>{html.escape(tb_string)}</pre>"
+    )
+
+    # Finally, send the message
+    await context.bot.send_message(
+        chat_id=getenv("OWNER_ID"), text=message, parse_mode=ParseMode.HTML
+    )
+
+
 def main() -> None:
     global db
     load_dotenv()
@@ -79,6 +114,7 @@ def main() -> None:
     app.add_handler(CommandHandler('removeclass', removeclass))
     app.add_handler(CommandHandler('listclasses', listclasses))
     app.add_handler(MessageHandler(filters.COMMAND, unknown))
+    app.add_error_handler(error_handler)
     updater_context: update_substitutions.CustomContext = update_substitutions.CustomContext(db)
     app.job_queue.run_repeating(update_substitutions.update, 5*60, data=updater_context)
     app.run_polling()
